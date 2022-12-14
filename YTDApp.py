3ded83d7e2240facd94269b6c69e744217bfdff6
Tilak Vijayaraghavan <stvraghavan@gmail.com>
2022-12-14 10:30:49 +0530

    enabled download

--- YTDApp.py
+++ YTDApp.py
@@ -10,14 +10,25 @@ yt = YouTube(url=link)
 st.title(yt.title)
 st.image(yt.thumbnail_url)
 
-# st.write(yt.streams.filter(adaptive=True))
-
 video_res = [i.resolution for i in yt.streams.filter(adaptive=True,file_extension='mp4')]
 video_res = set(video_res)
 
-
-st.write(video_res)
-# for i in yt.streams.filter(adaptive=True,file_extension='mp4'):
-#     st.write(str(i),"\n")
+# video_fps = [i.fps for i in yt.streams.filter(adaptive=True,file_extension='mp4')]
+# video_fps = set(video_fps)
+# st.write(video_fps)
 
 #Downloads section
+
+#Download function
+def Download(url, args):
+    yt = YouTube(url)
+
+    target = yt.streams.filter(adaptive=True).get_by_resolution(args)
+    target.download()
+#Download buttons
+for i in video_res:
+    try:
+        st.download_button(label="Download quality "+i,on_click=Download(link,i))
+    except:
+        pass
+
