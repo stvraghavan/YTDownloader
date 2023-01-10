724927e09228c1ed6777f8bcb7ac2029c4f87f19
Tilak <stvraghavan@gmail.com>
2023-01-10 14:09:25 +0530

    code redefining

--- YTDApp.py
+++ YTDApp.py
@@ -1,34 +1,44 @@
 import streamlit as st
 from pytube import YouTube
+from tqdm import tqdm
 
 #Input section
-st.header("YouTube Downloader App")
+st.header("YouTube Video / Audio  Downloader App")
 link = st.text_input("Paste the YouTube Video Link here")
 
 #Driver section
-yt = YouTube(url=link)
-st.title(yt.title)
-st.image(yt.thumbnail_url)
+try:
+    yt = YouTube(url=link)
+    st.title(yt.title)
+    st.image(yt.thumbnail_url)
 
-video_res = [i.resolution for i in yt.streams.filter(adaptive=True,file_extension='mp4')]
-video_res = set(video_res)
+    video_res = set([i.resolution for i in yt.streams.filter(progressive=True,file_extension='mp4')])
+    st.write(video_res)
+    audio_res = set([i.abr for i in yt.streams.filter(only_audio=True)])
+    st.write(audio_res)
 
-# video_fps = [i.fps for i in yt.streams.filter(adaptive=True,file_extension='mp4')]
-# video_fps = set(video_fps)
-# st.write(video_fps)
+    #Downloads section
 
-#Downloads section
 
-#Download function
-def Download(url, args):
-    yt = YouTube(url)
+    #Download function
+    def dwl(file_type,quality):
+        st.write(file_type)
 
-    target = yt.streams.get_by_resolution(args)
-    target.download()
-#Download buttons
-for i in video_res:
-    try:
-        st.button(label="Download quality "+i,on_click=Download(link,i))
-    except:
-        pass
-
+    st.header("Available download formats")
+    st.form:
+    
+    
+    for i in tqdm(video_res):
+        try:
+            if(st.button(label="Download quality "+i,key = "video")):
+                dwl("video",i)
+        except:
+            pass
+    for i in tqdm(audio_res):
+        try:
+            if(st.button(label="Download quality "+i,key = "video")):
+                dwl("audio",i)
+        except:
+            pass
+except:
+    pass
